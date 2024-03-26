e156d104b31d09bb7a320e6b875b0a66c3d42b34
marcelo <magalhaes1988@gmail.com>
2024-03-26 13:51:23 -0300

    Adcionado fornecedor obra

--- qualificacao/admin/fornecedor_admin.py
+++ qualificacao/admin/fornecedor_admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from qualificacao.models.cadastro_fornecedores import CadastroFornecedores, EnderecoFornecedor
 from qualificacao.models.fornecedor_obra import FornecedorObras
+from qualificacao.models.tipo_fornecimento import TipoFornecimento
 
 
 class EnderecoFornecedorAdmin(admin.ModelAdmin):
@@ -24,7 +25,14 @@ class FornecedorObrasAdmin(admin.ModelAdmin):
       field.name for field in FornecedorObras._meta.fields
     ]
 
+class TipoFornecimentoAdmin(admin.ModelAdmin):
+    readonly_fields = ('uuid',)
+    list_display = [
+      field.name for field in TipoFornecimento._meta.fields
+    ]
+
 
 admin.site.register(EnderecoFornecedor, EnderecoFornecedorAdmin)
 admin.site.register(CadastroFornecedores, CadastroFornecedoresAdmin)
-admin.site.register(FornecedorObras,FornecedorObrasAdmin)
+admin.site.register(FornecedorObras, FornecedorObrasAdmin)
+admin.site.register(TipoFornecimento, TipoFornecimentoAdmin)
