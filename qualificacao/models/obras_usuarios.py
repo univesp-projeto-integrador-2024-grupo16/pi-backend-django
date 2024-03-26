34f8d8fdb8d5801de0c9c36aad08b7765d83460a
marcelo <magalhaes1988@gmail.com>
2024-03-26 13:50:56 -0300

    Adcionado soft delete

--- qualificacao/models/obras_usuarios.py
+++ qualificacao/models/obras_usuarios.py
@@ -17,3 +17,25 @@ class ObrasUsuario(models.Model):
     )
     auth_user = models.ManyToManyField(User)
     obras = models.ManyToManyField(CadastroObras)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    deletado_em = models.DateTimeField(blank=True, null=True, default=None)
+    deletado = models.BooleanField(default=False)
+
+    def delete(self, *args, **kwargs):
+        if self.deletado:
+            models.Model.delete(self)
+        else:
+            self.deletado = True
+            self.deletado_em = datetime.now()
+            self.save()
+
+    def save(self, *args, **kwargs):
+        if self.deletado:
+            self.deletado = True
+            self.deletado_em = datetime.now()
+        else:
+            self.deletado = False
+            self.deletado_em = None
+
+        super(ObrasUsuario, self).save(*args, **kwargs)
